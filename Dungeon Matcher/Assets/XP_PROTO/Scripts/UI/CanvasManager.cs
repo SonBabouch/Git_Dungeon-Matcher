e99a5e647f9d02cab0bf003d49aa5bea94108c9f
Xanthin <x.paris@rubika-edu.com>
2021-02-22 18:15:57 +0100

    Correction + Build upToDate

--- Dungeon Matcher/Assets/XP_PROTO/Scripts/UI/CanvasManager.cs
+++ Dungeon Matcher/Assets/XP_PROTO/Scripts/UI/CanvasManager.cs
@@ -18,28 +18,27 @@ namespace Management
         //navigation
         public void GoToList()
         {
-            //fullCanvas.GetComponent<Animator>().SetInteger("State",2);
+            fullCanvas.GetComponent<Animator>().SetInteger("State",2);
             GameManager.currentGameState = GameManager.gameState.List;
-            Debug.Log(GameManager.currentGameState);
-            GameManager.Instance.GetComponent<CameraController>().MoveCamera();
+            //GameManager.Instance.GetComponent<CameraController>().MoveCamera();
         }
         public void GoToMatch()
         {
-            //fullCanvas.GetComponent<Animator>().SetInteger("State", 1);
+            fullCanvas.GetComponent<Animator>().SetInteger("State", 1);
             GameManager.currentGameState = GameManager.gameState.Match;
-            GameManager.Instance.GetComponent<CameraController>().MoveCamera();
+            //GameManager.Instance.GetComponent<CameraController>().MoveCamera();
         }
         public void GoToShop()
         {
-            //fullCanvas.GetComponent<Animator>().SetInteger("State", 0);
+            fullCanvas.GetComponent<Animator>().SetInteger("State", 0);
             GameManager.currentGameState = GameManager.gameState.Shop;
-            GameManager.Instance.GetComponent<CameraController>().MoveCamera();
+            //GameManager.Instance.GetComponent<CameraController>().MoveCamera();
         }
         public void GoToBag()
         {
-            //fullCanvas.GetComponent<Animator>().SetInteger("State", 3);
+            fullCanvas.GetComponent<Animator>().SetInteger("State", 3);
             GameManager.currentGameState = GameManager.gameState.Bag;
-            GameManager.Instance.GetComponent<CameraController>().MoveCamera();
+            //GameManager.Instance.GetComponent<CameraController>().MoveCamera();
         }
 
     }
