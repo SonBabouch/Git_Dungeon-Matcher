2a3d1b8510a6e8e963c9ca689d63090432fcb0cb
PARIS Xanthin <x.paris@rubika-edu.com>
2021-03-02 16:17:22 +0100

    Continuer travail

---
+++ Dungeon Matcher/Assets/Script/Xanthin/BagButtonBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Management;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script makes the behaviour of the BagButton.
+/// It makes the Fonction : Details / Use / Upgrade Working.
+/// </summary>
+public class BagButtonBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject detailsButton;
+    [SerializeField] private GameObject equipButton;
+    [SerializeField] private GameObject upgradeButton;
+
+    private void Start()
+    {
+        detailsButton.SetActive(false);
+        equipButton.SetActive(false);
+        upgradeButton.SetActive(false);
+    }
+
+
+    public void Details()
+    {
+
+    }
+
+    public void Use()
+    {
+
+    }
+
+    public void Upgrade()
+    {
+
+    }
+}
