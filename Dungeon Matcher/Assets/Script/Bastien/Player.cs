1c75d886da16457a2b0a670076d5f307f0428e72
Bastien <B.prigent@rubika-edu.com>
2021-03-04 18:55:29 +0100

    Add a player

---
+++ Dungeon Matcher/Assets/Script/Bastien/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance;
+    public List<Skill> playerSkills;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
