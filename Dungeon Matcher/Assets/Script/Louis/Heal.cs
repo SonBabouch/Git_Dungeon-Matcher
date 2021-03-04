8fa539fb6c4628d9f9c8bd58c7af9e30da264142
Louis Vitrant <l.vitrant@rubika-edu.com>
2021-03-04 23:35:56 +0100

    J'ai tenté quelque chose x Fatigue

---
+++ Dungeon Matcher/Assets/Script/Louis/Heal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Monster;
+
+public class Heal : Skill
+{
+    [SerializeField]
+    private MonsterToken monster;
+    private MonsterToken monsterSide;
+    [SerializeField]
+    private GameObject owner;
+    [SerializeField]
+    private float healthValue;
+
+    public override void Initialize(GameObject obj)
+    {
+        owner = obj;
+        monster = owner.GetComponent<MonsterToken>();
+    }
+
+    public override void Use()
+    {
+        /*switch (monster.side)
+        {
+            case 0:
+                monster.health -= healthValue;
+                break;
+            case 1:
+                Player.health -= healthValue;
+                break;
+            default:
+                Debug.Log("Default");
+                break;
+        }*/
+        //Switch permettant de soigner l'ennemi ou le joueur en fonction de qui lance le sort. Proposition non-définie. Enemy = 0 et Ally = 1 dans le script MonsterToken.
+
+        monster.health -= healthValue;
+    }
+}
+
