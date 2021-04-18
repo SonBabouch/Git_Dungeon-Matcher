31879e3834875ce74006d144d171cf9ef6e593a6
Xanthin <x.paris@rubika-edu.com>
2021-04-18 18:44:11 +0200

    Update Ui

--- Dungeon Matcher/Assets/_OFFICIEL/Script/UI/Combat/EnemyUi.cs
+++ Dungeon Matcher/Assets/_OFFICIEL/Script/UI/Combat/EnemyUi.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EnemyUi : MonoBehaviour
 {
     public Image enemyHealthBar;
     public Image enemyEnergyBar;
+
+    public Image profilPicture;
+    public Text monsterName;
 
     private void Update()
     {
@@ -14,6 +18,9 @@ public class EnemyUi : MonoBehaviour
         {
             enemyHealthBar.fillAmount = Enemy.Instance.health / Enemy.Instance.maxHealth;
             enemyEnergyBar.fillAmount = Enemy.Instance.energy / Enemy.Instance.maxEnergy;
+
+            monsterName.text = Enemy.Instance.currentMonster.GetComponent<MonsterToken>().monsterName;
+            profilPicture.sprite = Enemy.Instance.currentMonster.GetComponent<MonsterToken>().profilPicture;
         }
     }
 }
