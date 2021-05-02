01a4ca40f78def5ec24ce2a9b844cc1a0514e18e
Xanthin <x.paris@rubika-edu.com>
2021-05-03 01:28:18 +0200

    Message appering

--- Dungeon Matcher/Assets/TextBulle.cs
+++ Dungeon Matcher/Assets/TextBulle.cs
@@ -3,30 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class TextBulle : MonoBehaviour
 {
-    public int currentIndex = 0;
+    [SerializeField] private GameObject blackShader;
+    [SerializeField] private GameObject highlightPoint;
+    [SerializeField] private Button skipButton;
 
     [TextArea]
     public string[] messages;
+    private string currentText;
 
     public TextMeshProUGUI bubbleText;
 
+    public bool isTyping = false;
+    public int currentIndex = 0;
+
+    public enum MessageStatement {typing, needToSkip, needNextMessage}
+    public MessageStatement statement;
 
 
-
-
-
-
-    public IEnumerator AffichageMessage(float delay, string fullText,string currentText)
+    private void Start()
     {
-        for (int i = 0; i < fullText.Length; i++)
+        statement = MessageStatement.needNextMessage;
+        AffichageMessage();
+    }
+
+    public void ResetButton()
+    {
+        skipButton.onClick.RemoveAllListeners();
+    }
+
+    public void SkipMessage()
+    {
+        if (statement == MessageStatement.typing)
         {
-            currentText = fullText.Substring(0, i);
+            StopCoroutine(AffichageMessageEnum(0.05f, messages[currentIndex]));
+            currentText = messages[currentIndex];
             bubbleText.text = currentText;
-            yield return new WaitForSeconds(delay);
+            statement = MessageStatement.needNextMessage;
+            skipButton.onClick.AddListener(AffichageMessage);
         }
+    }
+
+    //Le mettre lorsque le joueur appuie sur le bouton.
+     void AffichageMessage()
+     {
+        ResetButton();
+
+        if(currentIndex == messages.Length)
+        {
+            //Appeler la Méthode pour finir;
+            skipButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            skipButton.onClick.AddListener(SkipMessage);
+            StartCoroutine(AffichageMessageEnum(0.05f, messages[currentIndex]));
+        }
+     }
+
+    public IEnumerator AffichageMessageEnum(float delay, string fullText)
+    {
+        statement = MessageStatement.typing;
+        for (int i = 0; i < fullText.Length; i++)
+        { 
+            if(statement == MessageStatement.typing)
+            {
+                currentText = fullText.Substring(0, i);
+                bubbleText.text = currentText;
+                yield return new WaitForSeconds(delay);
+            }
+        }
+        currentIndex++;
+        statement = MessageStatement.needNextMessage;
     }
 
 }
