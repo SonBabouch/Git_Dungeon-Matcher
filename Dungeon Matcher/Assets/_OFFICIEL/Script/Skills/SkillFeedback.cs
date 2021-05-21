af05d94452b08ac779ef3d2948b2701d0fab6d9f
Bastien <b.prigent@rubika-edu.com>
2021-05-21 17:01:41 +0200

    Start feedback heal

---
+++ Dungeon Matcher/Assets/_OFFICIEL/Script/Skills/SkillFeedback.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillFeedback : MonoBehaviour
+{
+    public GameObject enemiHeal;
+    public GameObject playerHeal;
+
+
+    public IEnumerator HealFeedback()
+    {
+        switch(Skill.monsterSide)
+        {
+            case Skill.monsterSide.Ally:
+                playerHeal.SetActive(true);
+                yield return new WaitForSeconds(0.1f)
+                break;
+        }
+    }
+}
